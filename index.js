4626618731f278db4bce4891772b528a5591160a
Sean Schuchman <sean@innovativetimingsystems.com>
2025-04-03 13:25:49 -0500

    Test simple express server

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
-
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
